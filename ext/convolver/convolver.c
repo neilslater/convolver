845f613c40fa8ccdf2b443b2d8074235d3e9e346
Neil Slater <neil@aframe.com>
2013-10-14 17:55:41 +0100

    Creating NArray of correct dimensions given inputs

--- ext/convolver/convolver.c
+++ ext/convolver/convolver.c
@@ -7,7 +7,42 @@
 VALUE Convolver = Qnil;
 
 static VALUE narray_convolve( VALUE self, VALUE a, VALUE b ) {
-  return Qnil;
+  struct NARRAY *na_a, *na_b, *na_c;
+  volatile VALUE val_a, val_b; val_c;
+  int target_rank, i;
+  int target_shape[16];
+
+  val_a = na_cast_object(a, NA_SFLOAT);
+  GetNArray( val_a, na_a );
+
+  val_b = na_cast_object(b, NA_SFLOAT);
+  GetNArray( val_b, na_b );
+
+  if ( na_a->rank < na_b->rank ) {
+    rb_raise( rb_eArgError, "narray b must have equal or lower rank than narray a" );
+  }
+
+  if ( na_a->rank < na_b->rank ) {
+    rb_raise( rb_eArgError, "narray a must have equal rank to narray b (temporary restriction)" );
+  }
+
+  if ( na_a->rank > 16 ) {
+    rb_raise( rb_eArgError, "exceeded maximum narray rank for convolve of 16" );
+  }
+
+  target_rank = na_a->rank;
+
+  for ( i = 0; i < target_rank; i++ ) {
+    target_shape[i] = na_a->shape[i] - na_b->shape[i] + 1;
+    if ( target_shape[i] < 1 ) {
+      rb_raise( rb_eArgError, "narray b is bigger in one or more dimensions than narray a" );
+    }
+  }
+
+  val_c = na_make_object( NA_SFLOAT, target_rank, target_shape, CLASS_OF( val_a ) );
+  GetNArray( val_c, na_c );
+
+  return val_c;
 }
 
 void Init_convolver() {
--- spec/convolver_spec.rb
+++ spec/convolver_spec.rb
@@ -7,6 +7,7 @@ describe Convolver do
       b = NArray[1.3, -0.5]
       c = Convolver.convolve( a, b )
       c.should be_a NArray
+      p c
       c[0].should be_within(1e-10).of 0.19
       c[1].should be_within(1e-10).of 0.27
     end
