932b0c92f31e211ed911750c64bcce39ba8d8e83
Neil Slater <neil@aframe.com>
2013-10-20 18:18:01 +0100

    Run a single layer of fully-connected MLP (without the transfer function)

--- spec/convolver_spec.rb
+++ spec/convolver_spec.rb
@@ -95,6 +95,12 @@ describe Convolver do
       thresholds = NArray[ 0.0, 0.0 ]
       outputs = Convolver.nn_run_layer( inputs, weights, thresholds );
       outputs.should be_narray_like NArray[ -0.5, 0.5 ]
+
+      inputs = NArray[ 0.3, -0.4, 0.8, -0.7 ]
+      weights = NArray[ [ 1.0, 0.25, 0.5, -0.5 ], [ -1.0, -0.25, -0.5, 0.5 ] ]
+      thresholds = NArray[ 0.0, 0.0 ]
+      outputs = Convolver.nn_run_layer( inputs, weights, thresholds );
+      outputs.should be_narray_like NArray[ 0.95, -0.95 ]
     end
   end
 end
