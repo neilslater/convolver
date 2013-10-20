e56e46f2f699e213f36c7ea79a63bcb490dc5ce4
Neil Slater <neil@aframe.com>
2013-10-20 15:49:26 +0100

    Start of raw NN layer function

--- ext/convolver/convolver.c
+++ ext/convolver/convolver.c
@@ -63,7 +63,7 @@ inline void calc_co_increment( int rank, int *outer_shape, int *inner_shape, int
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  Convolve method 4.
+//  Convolve
 //
 //    Benchmark: 640x480 image, 8x8 kernel, 1000 iterations. 11.54 seconds. Score: 63
 //
@@ -128,6 +128,55 @@ void convolve_raw(
   return;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Neural net
+//
+//    Benchmark: 1024 inputs, 256 outputs. 1000 iterations.
+//
+//
+
+void nn_run_layer_raw(
+    int in_size, int out_size,
+    float *in_ptr, float *weights, float *thresholds, float *out_ptr ) {
+  int i, j, in_aligned_size, out_aligned_size, offset;
+  __m128 simd_x, simd_y, simd_t;
+
+  in_aligned_size = 4 * ( in_size/4 );
+  out_aligned_size = 4 * ( out_size/4 );
+
+  // Calculate activation
+  for ( i = 0; i < out_size; i++ ) {
+
+    float t = 0.0;
+    simd_t = _mm_setzero_ps();
+    offset = i * in_size;
+
+    // Use SIMD for all the aligned values in groups of 4
+    for ( j = 0; j < in_aligned_size; j +=4 ) {
+      simd_x = _mm_load_ps( in_ptr + j );
+      simd_y = _mm_loadu_ps( weights + (offset + j) );
+      simd_x = _mm_mul_ps( simd_x, simd_y );
+      simd_t = _mm_add_ps( simd_x, simd_t );
+    }
+
+    // Complete any remaining 1,2 or 3 items one at a time
+    for ( j = in_aligned_size; j < in_size; j++ ) {
+      t += in_ptr[ j ] * weights[ offset + j ];
+    }
+
+    out_ptr[i] = simd_t[0] + simd_t[1] + simd_t[2] + simd_t[3] + t;
+  }
+
+  // Apply thresholds, run ReLU transfer function
+  for ( i = 0; i < out_aligned_size; i += 4 ) {
+    simd_x = _mm_load_ps( out_ptr + i );
+    simd_y = _mm_load_ps( thresholds + i );
+    simd_x = _mm_sub_ps( simd_x, simd_y );
+  }
+
+  return;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
