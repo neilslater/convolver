aa060192765058082dd9c121e13021299eb0b3bd
Neil Slater <neil@aframe.com>
2013-10-23 09:11:46 +0100

    Make FFTW3 convolve stand-alone whilst working on it

--- benchmarks/convolve_fftw3_spec.rb
+++ benchmarks/convolve_fftw3_spec.rb
@@ -1,4 +1,44 @@
-require 'helpers'
+require 'fftw3'
+
+# Matcher compares NArrays numerically
+RSpec::Matchers.define :be_narray_like do |expected_narray|
+  match do |given|
+    @error = nil
+    if ! given.is_a?(NArray)
+      @error = "Wrong class."
+    elsif given.shape != expected_narray.shape
+      @error = "Shapes are different."
+    else
+      d = given - expected_narray
+      difference =  ( d * d ).sum / d.size
+      if difference > 1e-10
+        @error = "Numerical difference with mean square error #{difference}"
+      end
+    end
+    @given = given.clone
+
+    if @error
+      @expected = expected_narray.clone
+    end
+
+    ! @error
+  end
+
+  failure_message_for_should do
+    "NArray does not match supplied example. #{@error}
+    Expected: #{@expected.inspect}
+    Got: #{@given.inspect}"
+  end
+
+  failure_message_for_should_not do
+    "NArray is too close to unwanted example.
+    Unwanted: #{@given.inspect}"
+  end
+
+  description do |given, expected|
+    "numerically very close to example"
+  end
+end
 
 module Convolver
   def self.convolve_fftw3 orig_a, orig_b
