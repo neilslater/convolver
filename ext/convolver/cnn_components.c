2ed4d9a83967872e40bdf0de47d564f76301568c
Neil Slater <neil@aframe.com>
2013-10-29 18:54:32 +0000

    Increment indices rather than re-calculate for max_pooling.

--- ext/convolver/cnn_components.c
+++ ext/convolver/cnn_components.c
@@ -26,7 +26,26 @@ inline void xna_inline_pos_to_idxs( int rank, int *shape, int pos, int *idxs ) {
   return;
 }
 
-inline int size_from_shape( int rank, int *shape ) {
+
+// Starts indices
+inline void indices_reset( int rank, int *indices ) {
+  int i;
+  for ( i = 0; i < rank; i++ ) { indices[i] = 0; }
+  return;
+}
+
+// Increments indices
+inline int indices_inc( int rank, int *shape, int *indices ) {
+  int i = 0;
+  while ( indices[i]++ > shape[i] - 2 ) {
+    indices[i] = 0;
+    i++;
+  }
+  return i;
+}
+
+
+inline int size_from_shape2( int rank, int *shape ) {
   int size = 1;
   int i;
   for ( i = 0; i < rank; i++ ) { size *= shape[i]; }
@@ -96,21 +115,23 @@ void max_pool_raw( int rank, int *input_shape, float *input_ptr,
   double max;
 
   for ( i = 0; i < rank; i++ ) { pool_shape[i] = pool_by; }
-  pool_size = size_from_shape( rank, pool_shape );
-  output_size = size_from_shape( rank, output_shape );
+  pool_size = size_from_shape2( rank, pool_shape );
+  output_size = size_from_shape2( rank, output_shape );
 
+  indices_reset( rank, output_idx );
   for (i = 0; i < output_size; i++ ) {
-    xna_inline_pos_to_idxs( rank, output_shape, i, output_idx );
     max = -1e30;
+    indices_reset( rank, pool_idx );
     for (j = 0; j < pool_size; j++ ) {
-      xna_inline_pos_to_idxs( rank, pool_shape, j, pool_idx );
       for ( k = 0; k < rank; k++ ) { input_idx[k] = output_idx[k] * tile_by + pool_idx[k]; }
       pos = xna_inline_idxs_to_pos( rank, input_shape, input_idx );
       if ( pos >= 0 && input_ptr[pos] > max ) {
         max = input_ptr[pos];
       }
+      indices_inc( rank, pool_shape, pool_idx );
     }
     output_ptr[i] = max;
+    indices_inc( rank, output_shape, output_idx );
   }
 
   return;
